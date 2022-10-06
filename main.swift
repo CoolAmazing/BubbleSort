6e39a51ffff98dd673a480dc636d00063b619391
Andrew Liu <andrew.liu@student.allenisd.org>
2022-10-05 22:06:19 -0500

    finished, ready for testing

--- main.swift
+++ main.swift
@@ -1,7 +1,32 @@
 var words = [String]()
 
+func readWords() -> [String] {
+    var line : String?
+    repeat {
+        line = readLine()
+        if line != nil {
+            words.append(line!)
+        }
+    } while line != nil
+    return words
+}
 
+func bubbleSort(_ array: [String]) -> [String] {
+    var a = readWords()
+    for _ in 0..<array.count - 1 {
+        for x in 0 ..< array.count - 1 {
+            if a[x] > a[x+1] {
+                let temp = a[x]
+                a[x] = a[x+1]
+                a[x+1] = temp
+            }
+        }
+    }
+    return a
+}
 
-func bubbleSort(unsortedIntegers: inout [String]) {
+let a = bubbleSort(words)
 
+for x in a {
+    print(x)
 }
